e251ec7d996e7901418017f509ed55eca36807e5
Bakdaulet <bakosh21345@gmail.com>
2021-03-04 22:59:41 +0600

    all

--- Scud/views.py
+++ Scud/views.py
@@ -1,8 +1,72 @@
 from testd.models import Staff,StaffSessions
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,JsonResponse
 from django.shortcuts import render,get_object_or_404
+from django.core import serializers
+
+import datetime
+date = datetime.date.today()
+day = date.strftime("%d")
+month = date.strftime("%B")
+
+
+def allStaff(request):
+    sessions = StaffSessions.objects.exclude(user=None)
+    data = {
+        "sessions":sessions,
+        "date":date
+    }
+
+    return render(request,'main/allStaff.html',data)
+
+def questions(request):
+    sessions = StaffSessions.objects.filter(user=None)
+    data = {
+        "sessions":sessions,
+        "date":date,
+        
+    }
+
+    return render(request,'main/questions.html',data)
+
+def detailStaff(request,id):
+
+    staff = Staff.objects.get(id=id)
+    sessions = staff.session.all()
+    data = {
+        "sessions":sessions,
+        "date":date,
+        'staff':staff
+    }
+
+    return render(request,'main/detail.html',data)
+
 
 def index(request):
+
+    sessions = StaffSessions.objects.filter(created_at__day=day).order_by('-created_at')
+
+    data = {
+        "sessions":sessions,
+        "date":date
+    }
+
+    return render(request,'main/main.html',data)
+
+def indexJson(request):
+    session = StaffSessions.objects.last()
+    if session.user == None:
+        user = "None"
+    else:
+        user = list(Staff.objects.values_list().get(id=session.user_id))
+
+
+    sessions = list(StaffSessions.objects.values_list().last())
+
+    return JsonResponse({'data':sessions,'user':user})
+
+
+
+def send(request):
 
     queryin = request.GET.get('rfidin')
     queryout = request.GET.get('rfidout')
@@ -13,18 +77,21 @@ def index(request):
 
             session = StaffSessions.objects.create(
                 uid = queryout,
-                check = 1
+                check = 1,
+                inout="Шықты",
+                user=user
             )
             session.save()
 
-            return HttpResponse(str(1) +"out")
+            return HttpResponse(str(1))
         except Staff.DoesNotExist:
             session = StaffSessions.objects.create(
                 uid = queryout,
-                check = 0
+                check = 0,
+                inout="Шықты"
             )
             session.save()
-            return HttpResponse(str(0)+"out")
+            return HttpResponse(str(0))
     elif queryin:
 
         try:
@@ -32,18 +99,21 @@ def index(request):
 
             session = StaffSessions.objects.create(
                 uid=queryin,
-                check=1
+                check=1,
+                inout="Кірді",
+                user=user
             )
             session.save()
 
-            return HttpResponse(str(1) + "in")
+            return HttpResponse(str(1))
         except Staff.DoesNotExist:
             session = StaffSessions.objects.create(
                 uid=queryin,
-                check=0
+                check=0,
+                inout="Кірді"
             )
             session.save()
-            return HttpResponse(str(0)+'in')
+            return HttpResponse(str(0))
     else:
         return HttpResponse("nothing")
     
--- testd/models.py
+++ testd/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+from django.utils import timezone
 
+class Spesilization(models.Model):
+
+    name = models.CharField('Название специлизаций',max_length=255)
 
 
 class Staff(models.Model):
 
     uid = models.CharField('Uid человека',max_length=255)
     name = models.CharField('Name',max_length=255)
+    lastname = models.CharField('Username',default="LastName",max_length=255)
+    spesilization = models.ForeignKey(Spesilization,blank=True,null=True,default=None,on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="ProfilePhotos/")
 
     def __str__(self):
 
@@ -17,9 +24,11 @@ class Staff(models.Model):
         verbose_name_plural = "Работники"
 
 class StaffSessions(models.Model):
-
+    user = models.ForeignKey(Staff,blank=True,null=True,default=None,on_delete=models.CASCADE,related_name="session")
     uid = models.CharField('Uid человека',max_length=255)
     check = models.BooleanField('Чек',default=0)
+    inout = models.CharField('Вход/Выход',max_length=255,default=0)
+    created_at = models.DateTimeField('Время',default=timezone.now)
 
     def __str__(self):
 
